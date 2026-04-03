44806b9bcdf792db90662b573ee083f2cfce8532
Pablo Nieto Hurtado <pablonietohurtado@Pablos-MacBook-Pro.local>
2026-04-03 22:29:21 +0200

    First Commit

--- springproject/src/main/java/com/springproject/springproject/SpringprojectApplication.java
+++ springproject/src/main/java/com/springproject/springproject/SpringprojectApplication.java
@@ -2,8 +2,9 @@ package com.springproject.springproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringprojectApplication {
 
 	public static void main(String[] args) {
