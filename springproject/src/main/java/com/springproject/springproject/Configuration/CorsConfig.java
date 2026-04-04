03864092da0c031d1d920a156d3c176bdf7c8668
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2026-04-04 19:19:24 +0200

    CorsConfiguration

---
+++ springproject/src/main/java/com/springproject/springproject/Configuration/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.springproject.springproject.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer
+
+    /*Needed for the configuration of the web and all the ENDPOINTS
+    * It is important to have a CORS for the origins of the request
+    * take into account that having a secure and reliable backend
+    * requires a strong CORS configuration*/
+
+{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("PUT", "GET", "POST", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
