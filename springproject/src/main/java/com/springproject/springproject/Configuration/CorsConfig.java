b672074f69c1e45dca0a7e5b9fe6e30e09d4414f
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2026-04-20 21:53:01 +0200

    Adjustment

--- springproject/src/main/java/com/springproject/springproject/Configuration/CorsConfig.java
+++ springproject/src/main/java/com/springproject/springproject/Configuration/CorsConfig.java
@@ -7,11 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer
 
-    /*Needed for the configuration of the web and all the ENDPOINTS
-    * It is important to have a CORS for the origins of the request
-    * take into account that having a secure and reliable backend
-    * requires a strong CORS configuration*/
-
 {
 
 
