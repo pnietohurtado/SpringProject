1010a5ad1113ab490dd2e8772255bd0247879986
Pablo Nieto Hurtado <pnietohurtado03@gmail.com>
2026-04-30 09:43:26 +0200

    Model class User

--- springproject/src/main/java/com/springproject/springproject/Dominio/DTO/Usuario.java
+++ springproject/src/main/java/com/springproject/springproject/Dominio/DTO/Usuario.java
@@ -19,4 +19,17 @@ public class Usuario {
         this.password = password;
     }
 
+    public String getEmail(){return this.email;}
+    public String getUsername(){return this.username;}
+    public String getFirstName(){return this.firstName;}
+    public String getLastName(){return this.lastName;}
+    public String getPassword(){return this.password;}
+
+    public void setEmail(String email){this.email = email; }
+    public void setUsername(String username){this.username = username; }
+    public void setFirstName(String firstName){this.firstName = firstName;}
+    public void setLastName(String lastName){this.lastName = lastName; }
+    public void setPassword(String password){this.password = password; }
+
+
 }
